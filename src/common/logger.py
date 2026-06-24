6329039d8965aec776ffdb214668d433a5c87218
Yamkela macwili <yamkela22y@gmail.com>
2026-06-24 14:43:10 +0200

    feat: establish common logging utilities

---
+++ src/common/logger.py
@@ -0,0 +1,19 @@
+"""Common logging utilities."""
+import logging
+import sys
+
+
+def get_logger(name: str) -> logging.Logger:
+    """Return a configured logger for the given module name."""
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(
+            logging.Formatter(
+                fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
+        )
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
